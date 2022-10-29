f0ba3698a905f389f3d9892b12fdd2bf420409ac
Alisson Luis <alissonluis104@yahoo.com.br>
2022-10-29 20:10:06 -0300

    Impar ou Par

---
+++ ImparOuPar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProjetoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.Write("Digite um numero: ");
+            double num = double.Parse(Console.ReadLine());
+
+            if (num%2 ==0) 
+            {
+                Console.WriteLine("O numero digitado é par");
+            }
+            else
+            {
+                Console.WriteLine("O numero digitado é impar ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
